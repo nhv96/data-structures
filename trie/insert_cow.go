99aa74ac835d4c5b15c5394232647aaff59a892b
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2024-08-04 17:08:09 +0700

    WIP: implement cow pattern

--- trie/insert_cow.go
+++ trie/insert_cow.go
@@ -7,25 +7,49 @@ func (t *Trie) InsertCOW(key string, value int) *Trie {
 	cur := t.Root
 
 	// create new root
-	newRoot := NewNode()
+	newTrie := NewTrie()
+	newParent := NewNode()
+	newTrie.Root = newParent
 
 	// find the node that need modified or to be inserted
 	for _, c := range key {
 		children, exist := cur.Children[string(c)]
 		if exist {
-			cur = children
+			// create a new children with mutated data
+			newchild := NewNode()
+
+			// point all the children of the current child to new child (as parent)
+			if len(children.Children) > 0 {
+				for k, node := range children.Children {
+					newchild.Children[k] = node
+				}
+			}
+
+			// link the newly created child node to parent node
+			newParent.Children[string(c)] = newchild
+
+			// link the existing nodes (except for the node with same key)
+			// to the new parent node
+			for k, node := range cur.Children {
+				if k != string(c) {
+					newParent.Children[k] = node
+				}
+			}
+
+			// move to the next inner node
+			cur = newchild
 		} else {
-			// create new node and assign its parent as the newRoot?
+			// create new node and assign its parent
 			node := NewNode()
-			newRoot.Children[string(c)] = node
-			cur = newRoot.Children[string(c)]
+			cur.Children[string(c)] = node
+
+			// move to the next inner node
+			cur = cur.Children[string(c)]
 		}
 	}
 
 	cur.IsTerminal = true
 	cur.Value = value
 
-	newTrie := NewTrie()
-	newTrie.Root = newRoot
 	return newTrie
 }
--- trie/insert_cow_test.go
+++ trie/insert_cow_test.go
@@ -5,7 +5,7 @@ import "testing"
 func Test_InsertCOW(t *testing.T) {
 	t.Parallel()
 
-	t.Run("insert", func(t *testing.T) {
+	t.Run("new node must be added", func(t *testing.T) {
 		trie0 := NewTrie()
 		trie1 := trie0.InsertCOW("a", 1)
 
@@ -23,6 +23,91 @@ func Test_InsertCOW(t *testing.T) {
 
 		if found0 != false || node0 != nil {
 			t.Errorf("Key \"%s\" is expected to be \"%v\", got \"%v\"", "a", false, found0)
+		}
+	})
+
+	t.Run("new node must be added and retain old nodes", func(t *testing.T) {
+		trie0 := NewTrie()
+		trie0.Insert("aa", 1)
+		trie0.Insert("ab", 2)
+		trie0.Insert("bc", 3)
+
+		trie1 := trie0.InsertCOW("ba", 4)
+
+		testSteps := []struct {
+			name         string
+			tree         *Trie
+			key          string
+			expectedVal  int
+			expectedTerm bool
+		}{
+			{
+				name:         "find in trie0 and must not find \"a\"",
+				tree:         trie0,
+				key:          "a",
+				expectedVal:  0,
+				expectedTerm: false,
+			},
+			{
+				name:         "find in trie0 and must find key \"aa\"",
+				tree:         trie0,
+				key:          "aa",
+				expectedVal:  1,
+				expectedTerm: true,
+			},
+			{
+				name:         "find in trie1 and must not find key \"a\"",
+				tree:         trie1,
+				key:          "a",
+				expectedVal:  0,
+				expectedTerm: false,
+			},
+			{
+				name:         "find in trie1 and must find key \"aa\"",
+				tree:         trie1,
+				key:          "aa",
+				expectedVal:  1,
+				expectedTerm: true,
+			},
+			{
+				name:         "find in trie1 and must find key \"ab\"",
+				tree:         trie1,
+				key:          "ab",
+				expectedVal:  2,
+				expectedTerm: true,
+			},
+			{
+				name:         "find in trie1 and must find \"bc\"",
+				tree:         trie1,
+				key:          "bc",
+				expectedVal:  3,
+				expectedTerm: true,
+			},
+			{
+				name:         "find in trie1 and must find \"ba\"",
+				tree:         trie1,
+				key:          "ba",
+				expectedVal:  4,
+				expectedTerm: true,
+			},
+			{
+				name:         "find in trie0 and must not find \"ba\"",
+				tree:         trie0,
+				key:          "ba",
+				expectedVal:  0,
+				expectedTerm: false,
+			},
+		}
+
+		for _, steps := range testSteps {
+			node, found := find(steps.tree, steps.key)
+			if found != steps.expectedTerm {
+				t.Errorf("Step '%s' expected to be %v, got %v", steps.name, steps.expectedTerm, found)
+			}
+
+			if found && node.Value != steps.expectedVal {
+				t.Errorf("Step '%s' expected to be %v, got %v", steps.name, steps.expectedVal, node.Value)
+			}
 		}
 	})
 }
